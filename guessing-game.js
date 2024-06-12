697cfdbc0850a25c79996be51952095e130c0381
000xzhou <xzhou92@gmail.com>
2024-06-12 16:07:39 -0400

    guessingGame

--- guessing-game.js
+++ guessing-game.js
@@ -1,3 +1,26 @@
-function guessingGame() {}
+function guessingGame() {
+  // random whole number between 0 and 99
+  let random = 60;
+  let guess = 0;
+  let gameover = false;
+
+  return function (num) {
+    // console.log(num);
+    guess++;
+    if (num < random) {
+      return `${num} is too low!`;
+    } else if (num > random) {
+      return `${num} is too high!`;
+    } else {
+      // stop guess .. how?
+      if (gameover) {
+        return `The game is over, you already won!`;
+      } else {
+        gameover = true;
+        return `You win! You found ${num} in ${guess} guesses.`;
+      }
+    }
+  };
+}
 
 module.exports = { guessingGame };
